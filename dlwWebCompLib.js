d4535bad52e3306f99101b99695732f17a066760
conradsbrain <121487603+conradsbrain@users.noreply.github.com>
2023-05-04 20:56:36 +0200

    Update dlwWebCompLib.js

--- dlwWebCompLib.js
+++ dlwWebCompLib.js
@@ -54,12 +54,14 @@ function render() {
 
 /* web component for Hero Banner Image */
 const heroElement = document.querySelector("hero-banner");
-const hbHeight = heroElement.dataset.imgHeight;
-const hbUrl = heroElement.dataset.imgUrl;
-const hbPosition = heroElement.dataset.imgPosition;
-heroElement.style.setProperty("--bg-hero-banner-image-height", hbHeight + "px");
-heroElement.style.setProperty("--bg-hero-banner-image-url", "url("+hbUrl+")");
-heroElement.style.setProperty("--bg-hero-banner-image-position", hbPosition);
+if (heroElement) {
+  const hbHeight = heroElement.dataset.imgHeight;
+  const hbUrl = heroElement.dataset.imgUrl;
+  const hbPosition = heroElement.dataset.imgPosition;
+  heroElement.style.setProperty("--bg-hero-banner-image-height", hbHeight + "px");
+  heroElement.style.setProperty("--bg-hero-banner-image-url", "url("+hbUrl+")");
+  heroElement.style.setProperty("--bg-hero-banner-image-position", hbPosition);
+}
 
 const templateHeroBanner = document.createElement('template');
 templateHeroBanner.innerHTML = `
