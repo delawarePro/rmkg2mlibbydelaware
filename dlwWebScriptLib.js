7cd7c0622ff5e29945aa29064328285f44b83840
conradsbrain <121487603+conradsbrain@users.noreply.github.com>
2023-05-08 16:39:31 +0200

    Update dlwWebScriptLib.js

--- dlwWebScriptLib.js
+++ dlwWebScriptLib.js
@@ -1,1 +1,48 @@
+let searchBtn = document.querySelector(".cc-search-btn");
+console.log(searchBtn);
+let closeBtn = document.querySelector(".cc-close-btn");
+let searchBox = document.querySelector(".cc-search-box");
+let searchBoxInput = document.querySelector(".cc-search-box-input");
+let searchBoxSubmitBtn = document.querySelector(".cc-submit-job-search-btn");
+let headerNavigation = document.querySelector(".cc-header-navigation");
+let mobileMenuToggle = document.querySelector(".cc-mobile-menu-toggle");
+let mobileMenuClose = document.querySelector(".cc-mobile-menu-close");
+let header = document.querySelector(".cc-header");
 
+searchBtn.onclick = function() {
+    searchBox.classList.add("active");
+    closeBtn.classList.add("active");
+    searchBoxSubmitBtn.classList.add("active");
+    searchBtn.classList.add("hide");
+    mobileMenuToggle.classList.add("hide");
+    mobileMenuClose.classList.add("hide");
+    header.classList.remove("open");
+}
+
+closeBtn.onclick = function() {
+    searchBox.classList.remove("active");
+    closeBtn.classList.remove("active");
+    searchBoxSubmitBtn.classList.remove("active");
+    searchBtn.classList.remove("active");
+    searchBtn.classList.remove("hide");
+    mobileMenuToggle.classList.remove("hide");
+    searchBoxInput.value = "";
+}
+
+mobileMenuToggle.onclick = function() {
+    header.classList.toggle("open");
+    searchBox.classList.remove("active");
+    closeBtn.classList.remove("active");
+    searchBtn.classList.remove("active");
+    mobileMenuClose.classList.remove("hide");
+    mobileMenuToggle.classList.add("hide");
+}
+
+mobileMenuClose.onclick = function() {
+    header.classList.toggle("open");
+    searchBox.classList.remove("active");
+    closeBtn.classList.remove("active");
+    searchBtn.classList.remove("active");
+    mobileMenuToggle.classList.remove("hide");
+    mobileMenuClose.classList.add("hide");
+}
