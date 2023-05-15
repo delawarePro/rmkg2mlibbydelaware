59a70f535e4472eb9b960b18adafaaf83f852cef
conradsbrain <121487603+conradsbrain@users.noreply.github.com>
2023-05-15 21:47:30 +0200

    Update dlwWebCompLib.js

--- dlwWebCompLib.js
+++ dlwWebCompLib.js
@@ -1,4 +1,12 @@
 /* web component for header */
+let ccSearchNavigation = [
+    {icon: '<a href="#" onclick="submitSearch()"><ion-icon name="search-outline" class="cc-submit-job-search-btn hide"></ion-icon>'},
+    {icon: '<ion-icon name="search-outline" class="cc-search-btn"></ion-icon>'},
+    {icon: '<ion-icon name="close-outline" class="cc-close-btn hide"></ion-icon>'},
+    {icon: '<ion-icon name="menu-outline" class="cc-mobile-menu-toggle"></ion-icon>'},
+    {icon: '<ion-icon name="close-outline" class="cc-mobile-menu-close hide"></ion-icon>'},
+]
+
 let navList = ccPrimaryNavDutch;
 let navLocale = "locale=nl_NL";
 
@@ -9,16 +17,25 @@ switch (languageToken) {
     break;
 }
 
+/*initiate header*/
 let ccHeader = document.createElement("header-nav");
 
-let headerLogo = '<a href="/?"' + navLocale + ' class="cc-logo"><span><img class="cc-logo-img" src="https://rmkcdn.successfactors.com/67fb2e9b/d1df3933-c965-478f-8bb2-3.svg"/></span></a>';
-let headerSearch = '<div class="cc-header-search"><span class="cc-icon"><a href="#" onclick="submitSearch()"><ion-icon name="search-outline" class="cc-submit-job-search-btn"></ion-icon></a><ion-icon name="search-outline" class="cc-search-btn"></ion-icon><ion-icon name="close-outline" class="cc-close-btn"></ion-icon></span></div>';
-let mobileHeaderMenuToggle = '<ion-icon name="menu-outline" class="cc-mobile-menu-toggle"></ion-icon>';
-let mobileHeaderMenuClose = '<ion-icon name="close-outline" class="cc-mobile-menu-close hide"></ion-icon>';
+let headerLogo = '<a href="/" class="cc-logo">Logo</a>';
+//let headerSearch = '<div class="cc-header-search"><span class="cc-icon"><a href="#" onclick="submitSearch()"><ion-icon name="search-outline" class="cc-submit-job-search-btn hide"></ion-icon></a><ion-icon name="search-outline" class="cc-search-btn"></ion-icon><ion-icon name="close-outline" class="cc-close-btn hide"></ion-icon></span></div>';
+//let mobileHeaderMenuToggle = '<ion-icon name="menu-outline" class="cc-mobile-menu-toggle"></ion-icon>';
+//let mobileHeaderMenuClose = '<ion-icon name="close-outline" class="cc-mobile-menu-close hide"></ion-icon>';
 let headerSearchBox = '<div class="cc-search-box"><form name="keywordsearch" class="cc-search-form" method="get" role="search" action="/search/"><input name="createNewAlert" type="hidden" value="false"><input type="text" class="cc-search-box-input" placeholder="Zoek jobs" name="q" maxlength="50" aria-label="Zoek op trefwoord"></form></div>';
-ccHeader.innerHTML = '<header class="cc-header">' + headerLogo + '<div class="cc-header-group">' + '<ul class="cc-header-navigation" data-navlist></ul>' + headerSearch + mobileHeaderMenuToggle + mobileHeaderMenuClose + '</div>' + headerSearchBox + '</header>';
+//ccHeader.innerHTML = '<header class="cc-header">' + headerLogo + '<div class="cc-header-group">' + '<ul class="cc-header-navigation" data-navlist></ul>' + headerSearch + mobileHeaderMenuToggle + mobileHeaderMenuClose + '</div>' + headerSearchBox + '</header>';
+ccHeader.innerHTML = '<header class="cc-header">' + headerLogo + '<div class="cc-header-group">' + '<ul class="cc-header-navigation" data-navlist></ul><ul class="cc-header-search" data-navsearchiconlist></ul></div>' + headerSearchBox + '</header>';
 document.body.insertBefore(ccHeader, document.body.firstChild);
 
+/*web component for header*/
+let navList = ccPrimaryNavDutch;
+switch (languageToken) {
+  case "fr":
+    navList = ccPrimaryNavFrench;
+    break;
+}
 let templateCcHeader = document.createElement('template');
 templateCcHeader.innerHTML = `
 <slot></slot>
@@ -36,16 +53,23 @@ customElements.define('header-nav', HeaderNav)
 function render() {
     const navContainer = document.querySelector('[data-navlist]');
     navList.forEach(navItem => {
-        const navElement = document.createElement('li')
-        let navElementLink = document.createElement('a')
-        let navElementLinkText = document.createTextNode(navItem.title)
-        navElementLink.appendChild(navElementLinkText)
-        navElementLink.href = navItem.url
-        navElementLink.title = navItem.title
-        navElementLink.target = navItem.target
-        navElement.appendChild(navElementLink)
-        navContainer.appendChild(navElement)
-    })
+        const navElement = document.createElement('li');
+        let navElementLink = document.createElement('a');
+        let navElementLinkText = document.createTextNode(navItem.title);
+        navElementLink.appendChild(navElementLinkText);
+        navElementLink.href = navItem.url;
+        navElementLink.title = navItem.title;
+        navElementLink.target = navItem.target;
+        navElement.appendChild(navElementLink);
+        navContainer.appendChild(navElement);
+    });
+
+    const navSearchContainer = document.querySelector('[data-navsearchiconlist]');
+    ccSearchNavigation.forEach(ccSearchNavigationItem => {
+        const navElementIcon = document.createElement('li');
+        navElementIcon.innerHTML = ccSearchNavigationItem.icon;
+        navSearchContainer.appendChild(navElementIcon);
+    });
 }
 
 
