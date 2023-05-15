abda10c10a77c9b8dd11baf4ca51df44966fe4c1
conradsbrain <121487603+conradsbrain@users.noreply.github.com>
2023-05-15 22:12:09 +0200

    Update dlwWebCompLib.js

--- dlwWebCompLib.js
+++ dlwWebCompLib.js
@@ -20,7 +20,7 @@ switch (languageToken) {
 /*initiate header*/
 let ccHeader = document.createElement("header-nav");
 
-let headerLogo = '<a href="/" class="cc-logo">Logo</a>';
+let headerLogo = '<a href="/" class="cc-logo"><img src="https://rmkcdn.successfactors.com/67fb2e9b/d1df3933-c965-478f-8bb2-3.svg" title="Brussels Airport Company" /></a>';
 //let headerSearch = '<div class="cc-header-search"><span class="cc-icon"><a href="#" onclick="submitSearch()"><ion-icon name="search-outline" class="cc-submit-job-search-btn hide"></ion-icon></a><ion-icon name="search-outline" class="cc-search-btn"></ion-icon><ion-icon name="close-outline" class="cc-close-btn hide"></ion-icon></span></div>';
 //let mobileHeaderMenuToggle = '<ion-icon name="menu-outline" class="cc-mobile-menu-toggle"></ion-icon>';
 //let mobileHeaderMenuClose = '<ion-icon name="close-outline" class="cc-mobile-menu-close hide"></ion-icon>';
