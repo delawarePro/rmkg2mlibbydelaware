29775db11484a851e1798e53b5d4f819dc7b374d
conradsbrain <121487603+conradsbrain@users.noreply.github.com>
2023-12-22 16:01:40 +0100

    Update dlwWebCompLib.rc.v1.2.js
    Fix scrollwidth

--- dlwWebCompLib.rc.v1.2.js
+++ dlwWebCompLib.rc.v1.2.js
@@ -1795,7 +1795,8 @@ if (wrapper) {
     carousel.scrollLeft = carousel.offsetWidth;
     carousel.classList.remove("cc-no-transition");
     console.log("carousel.scrollWidth: " + carousel.scrollWidth);
-
+    if (window.innerWidth > 800 && carousel.scrollWidth < 3709) carousel.scrollWidth = 3709;
+    
     arrowBtns.forEach(btn => {
         btn.addEventListener("click", (e) => {
             carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
