97b8baa4f1207e78c41b2b9cf7fdb4d38b9dbd98
conradsbrain <121487603+conradsbrain@users.noreply.github.com>
2024-01-25 10:14:55 +0100

    Update dlwWebCompLib.rc.v1.2.js
    encodeURIComponent io encodeURI

--- dlwWebCompLib.rc.v1.2.js
+++ dlwWebCompLib.rc.v1.2.js
@@ -1347,7 +1347,7 @@ class SearchTags extends HTMLElement {
             elementSearchTagSpan.innerText = '#' + searchTag;
             elementSearchTagLink.appendChild(elementSearchTagSpan);
 
-            elementSearchTagLink.href = '/search/?optionsFacetsDD_department='+ encodeURI(searchTag);
+            elementSearchTagLink.href = '/search/?optionsFacetsDD_department='+ encodeURIComponent(searchTag);
             elementSearchTagLink.title = searchTag;
             elementSearchTagLink.target = '_self';
 
